d9d4039279f2499a074881b11361752aafca02a7
hamsterdmin <dltmddud1122@naver.com>
2023-11-10 17:13:07 +0900

    start

---
+++ src/test/java/Study/demo/SpringStudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
